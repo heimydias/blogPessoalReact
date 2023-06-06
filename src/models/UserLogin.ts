6640e43540413ab15d9b9745dfa631d7ef603cd0
Heimy Dias <heimysantana@hotmail.com>
2023-06-06 14:45:25 -0300

    atualização

--- src/models/UserLogin.ts
+++ src/models/UserLogin.ts
@@ -1,13 +1,10 @@
-import Postagem from "./Postagem"
-
 interface UserLogin {
-    token: string
     id: number
     nome: string
     usuario: string
     senha: string
     foto: string
-    postagens?: Postagem[];
+    token: string
 }
 
 export default UserLogin;
